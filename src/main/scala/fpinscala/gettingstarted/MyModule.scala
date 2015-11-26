e92e5dd67367679aa21f76905fc8202076653257
Idel Pivnitskiy <Idel.Pivnitskiy@gmail.com>
2015-11-27 02:35:37 +0300

    2.2 isSorted polymorphic function

--- src/main/scala/fpinscala/gettingstarted/MyModule.scala
+++ src/main/scala/fpinscala/gettingstarted/MyModule.scala
@@ -2,6 +2,7 @@ package fpinscala.gettingstarted
 
 object MyModule {
 
+  // Exercise 1: Write a function to compute the n-th fibonacci number
   def fib(n: Int): Int = {
     if (n < 0)
       return -fib(-n)
@@ -12,6 +13,20 @@ object MyModule {
       else loop(n - 1, b, a + b)
     }
     loop(n, 0, 1)
+  }
+
+  // Exercise 2: Implement a polymorphic function to check whether an `Array[A]` is sorted
+  def isSorter[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    require(as.length > 1)
+
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      val res = ordered(as(n), as(n + 1))
+      if (n == 0) res
+      else if (res) loop(n - 1)
+      else false
+    }
+    loop(as.length - 2)
   }
 }
 
@@ -28,10 +43,24 @@ object TestMyModule {
       println("%2d: %2d == %2d".format(i, answers(i), res))
       require(answers(i) == res)
     }
-    println("---------------")
+    println("---------------\n")
+  }
+
+  def testIsSorter() = {
+    println("--- testIsSorter ---")
+    val arr = Array(
+      (Array(1, 2, 3, 4, 5), true),
+      (Array(1, 2, 7, 4, 5), false)
+    )
+    for ((as, require) <- arr) {
+      val res = isSorter(as, (x: Int, y: Int) => x <= y)
+      println("%5s == %5s: %s".format(res, require, as.mkString("[", ",", "]")))
+    }
+    println("--------------------\n")
   }
 
   def main(args: Array[String]): Unit = {
     testFib()
+    testIsSorter()
   }
 }
