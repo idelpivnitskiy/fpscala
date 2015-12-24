2c29b5bf45499d43075b5f4161b2b007d5452276
Idel Pivnitskiy <Idel.Pivnitskiy@gmail.com>
2015-12-25 01:01:53 +0300

    4.7: Implement `traverse` for Either

--- src/main/scala/fpinscala/errorhandling/EitherUsage.scala
+++ src/main/scala/fpinscala/errorhandling/EitherUsage.scala
@@ -8,6 +8,14 @@ object EitherUsage {
   def sequence[E, A](l: List[Either[E, A]]): Either[E, List[A]] = {
     l.foldRight[Either[E, List[A]]](Right(Nil))((h, t) => h.map2(t)(_ :: _))
   }
+
+  // Exercise 4.7: Implement `traverse`
+  def traverse[E, A, B](l: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    l.foldRight[Either[E, List[B]]](Right(Nil))((h, t) => f(h).map2(t)(_ :: _))
+  }
+  def sequenceViaTraverse[E, A](l: List[Either[E, A]]): Either[E, List[A]] = {
+    traverse(l)(x => x)
+  }
 }
 
 object EitherUsageTest {
@@ -24,8 +32,10 @@ object EitherUsageTest {
       printAndCheck(Left("empty"),        f(List(Right(1),      Left("empty"), Right(3))))
       printAndCheck(Left("empty"),        f(List(Right(1),      Right(2),      Left("empty"))))
       printAndCheck(Left("empty"),        f(List(Left("empty"))))
+      println()
     }
     test(sequence)
+    test(sequenceViaTraverse)
     println("--------------------\n")
   }
 
